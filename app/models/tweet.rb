ee8aa610bbcf7b0e7286086f266ccd6cc3ed6be4
Kendall <adkins.kendall90@gmail.com>
2017-08-18 16:10:06 -0400

    api key works! - victory number 1

--- app/models/tweet.rb
+++
@@ -1,3 +0,0 @@
-class Tweet < ActiveRecord::Base
-  has_many :pictures
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Photo.destroy_all
+Phrase.destroy_all
+
+require 'flickraw'
+
+FlickRaw.api_key=ENV['API_KEY']
+FlickRaw.shared_secret="429b7c1005fd02a8"
+
+list = flickr.photos.search tags: "mothballs, fire"
+
+puts list.length
+list.each do |x|
+  puts x.title
+end
